20efb676516fcdb01aa440cfd6f4082cd961e30d
dprensa1 <d.prensa1@gmail.com>
2015-12-10 18:36:45 -0400

    Seccion de Acciones, Mejoras en Roles, Empleados, Administracion y SharedView3

---
+++ TPV/Controllers/AccesosController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace TPV.Controllers
+{
+    public class AccesosController : Controller
+    {
+        // GET: Accesos
+        [Route("Accesos")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Accesos/Crear
+        [Route("Accesos/Crear")]
+        public ActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
---
+++ TPV/Models/Acceso.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace TPV.Models
+{
+    public class Acceso
+    {
+        public int AccesoID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public virtual ICollection<string> AccesosURL { get; set; }
+
+        public char Estado { get; set; }
+
+        //No se muestran en el formulario
+        public char Estatus { get; set; }
+
+        public System.DateTime FechaCreacion { get; set; }
+
+        public int UsuarioID { get; set; }
+
+        public virtual Usuario CreadoPor { get; set; }
+    }
+}
